c663aa1b4850066f2af4cb68637dace86814ce7b
MatAndrade01 <ltbmatheus141012@gmail.com>
2024-12-07 09:22:11 -0300

    Aprendo sobre variaveis

--- aula-06/index.js
+++ aula-06/index.js
@@ -1,2 +1,37 @@
-console.log('Wello world!');
-console.log('Estamos na aula 06!');clearInterval
+//Utilize  sempre o Camel Case no JavaScript!
+
+//JavaScript possui tipagem dinamica: Infere os tipos de dados.
+//JavaScript possui uma tipagem fraca
+
+var nomeDoProfessor = "Matheus Emanuel"; //string (texto): Utilize sempre em aspas!
+var idade = 22; //number
+var altura = 1.77;//number
+var estudando = true;//bollean (booleano: verdadeiro ou falso)
+
+console.log(nomeDoProfessor, typeof nomeDoProfessor);
+
+console.log(idade, typeof idade);
+
+console.log(altura, typeof altura);
+
+console.log(estudando, typeof estudando);
+
+var semConteudo; //decladrando uma variável;
+
+console.log(semConteudo);
+
+var curso = "Front-end em React",
+    topico = "JavaScript Básico I";
+
+console.log(curso, topico);
+
+// !Não utilizar o VAR nos seus códigos JavaScript
+
+let notaDoAluno = 10;
+// const mediaDoAluno = 8; Não é permitido
+
+notaDoAluno = 9;
+mediaDoAluno = 5;
+
+console.log(notaDoAluno);
+console.log(mediaDoAluno);
