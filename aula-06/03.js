fe92062dbb8a8af99ea09258abbf1b76d5b2ae33
MatAndrade01 <ltbmatheus141012@gmail.com>
2024-12-07 10:08:30 -0300

    Aprendendo Coerções!

--- aula-06/03.js
+++ aula-06/03.js
@@ -2,4 +2,34 @@ const prompt = require('readline-sync')
 
 const idade = prompt.question("Qual a sua idade ?");
 
-console.log(idade, typeof idade);
+const idadeNumber = Number(idade);
+
+console.log(idadeNumber, typeof idadeNumber);
+
+//Coerção Explícita (Coversão Manual).
+
+console.log(Number("X")); //Nan: Not a Number.
+
+console.log(String(10), typeof String(10));
+
+console.log(Boolean(0));
+
+//Coerção Implícita.
+
+console.log(1 + '1');
+console.log(5 + '10');
+console.log(10 - '5');
+console.log(10 * '5');
+console.log(10 / '5');
+
+let n = 1 + '1';//'11' String.
+
+n = n -1;//11 - 1 = 10 Number.
+
+console.log(n);
+
+console.log(2 + 3 + 4 + '5');// '95' String.
+
+console.log('5' + 2 + 3 + 4);// '5234' String.
+
+console.log('10' - '4' - '3' - 2 + '5');//'15' String
